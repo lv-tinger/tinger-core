23b89a7bfda4693b41c9d3459138a95b6daf4c6d
lv-tinger <110197104+lv-tinger@users.noreply.github.com>
2022-11-29 03:23:24 +0800

    ...

--- src/main/java/org/tinger/core/cache/CacheBuilder.java
+++
@@ -1,8 +0,0 @@
-package org.tinger.core.cache;
-
-/**
- * Created by tinger on 2022-11-13
- */
-public interface CacheBuilder {
-    CacheProvider build(CacheDriver driver, String name);
-}
---
+++ src/main/java/org/tinger/core/cache/CacheModule.java
@@ -0,0 +1,6 @@
+package org.tinger.core.cache;
+
+import org.tinger.core.apps.Module;
+
+public interface CacheModule extends Module<CacheProvider> {
+}
